e14195b99f8191540c645c4fbf449c728c060bde
FrancescoVenanti <francesco.venanti@gmail.com>
2024-02-13 14:31:00 +0100

    Aggiungere i file di progetto.

---
+++ Store be_l3/About.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Store_be_l3
+{
+    public partial class About : Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["ASPNET_COOKIE"] != null)
+            {
+                username_par.InnerText = "Ciao " + Request.Cookies["ASPNET_COOKIE"]["Username"];
+               
+            }
+            else
+            {
+                Response.Redirect("Default");
+            }
+        }
+
+        
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string pr1;
+            string pr2;
+            string pr3;
+            string pr4;
+
+            HttpCookie cookie = new HttpCookie("Prodotti");
+            cookie.Expires = DateTime.Now.AddDays(5);
+            cookie.Values["prpdotto1"] = prodotto1.InnerText;
+
+            if (Request.Cookies["Prodotti"] != null)
+            {
+                pr1 = Request.Cookies["Prodotti"]["Prodotto1"];
+                pr2 = Request.Cookies["Prodotti"]["Prodotto2"];
+                pr3 = Request.Cookies["Prodotti"]["Prodotto3"];
+                pr4 = Request.Cookies["Prodotti"]["Prodotto4"];
+
+                cookie.Values["prpdotto2"] = pr2;
+                cookie.Values["prpdotto3"] = pr3;
+                cookie.Values["prpdotto4"] = pr4;
+            }
+            
+            Response.Cookies.Add(cookie);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string pr1;
+            string pr2;
+            string pr3;
+            string pr4;
+
+            HttpCookie cookie = new HttpCookie("Prodotti");
+            cookie.Expires = DateTime.Now.AddDays(5);
+            cookie.Values["prpdotto2"] = prodotto2.InnerText;
+
+            if (Request.Cookies["Prodotti"] != null)
+            {
+                pr1 = Request.Cookies["Prodotti"]["Prodotto1"];
+                pr2 = Request.Cookies["Prodotti"]["Prodotto2"];
+                pr3 = Request.Cookies["Prodotti"]["Prodotto3"];
+                pr4 = Request.Cookies["Prodotti"]["Prodotto4"];
+
+                cookie.Values["prpdotto1"] = pr1;
+                cookie.Values["prpdotto3"] = pr3;
+                cookie.Values["prpdotto4"] = pr4;
+            }
+
+            Response.Cookies.Add(cookie);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            string pr1;
+            string pr2;
+            string pr3;
+            string pr4;
+
+            HttpCookie cookie = new HttpCookie("Prodotti");
+            cookie.Expires = DateTime.Now.AddDays(5);
+            cookie.Values["prpdotto3"] = prodotto3.InnerText;
+
+            if (Request.Cookies["Prodotti"] != null)
+            {
+                pr1 = Request.Cookies["Prodotti"]["Prodotto1"];
+                pr2 = Request.Cookies["Prodotti"]["Prodotto2"];
+                pr3 = Request.Cookies["Prodotti"]["Prodotto3"];
+                pr4 = Request.Cookies["Prodotti"]["Prodotto4"];
+
+                cookie.Values["prpdotto2"] = pr2;
+                cookie.Values["prpdotto1"] = pr1;
+                cookie.Values["prpdotto4"] = pr4;
+            }
+
+            Response.Cookies.Add(cookie);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            string pr1;
+            string pr2;
+            string pr3;
+            string pr4;
+
+            HttpCookie cookie = new HttpCookie("Prodotti");
+            cookie.Expires = DateTime.Now.AddDays(5);
+            cookie.Values["prpdotto4"] = prodotto4.InnerText;
+
+            if (Request.Cookies["Prodotti"] != null)
+            {
+                pr1 = Request.Cookies["Prodotti"]["Prodotto1"];
+                pr2 = Request.Cookies["Prodotti"]["Prodotto2"];
+                pr3 = Request.Cookies["Prodotti"]["Prodotto3"];
+                pr4 = Request.Cookies["Prodotti"]["Prodotto4"];
+
+                cookie.Values["prpdotto2"] = pr2;
+                cookie.Values["prpdotto3"] = pr3;
+                cookie.Values["prpdotto1"] = pr1;
+            }
+
+            Response.Cookies.Add(cookie);
+        }
+    }
+}
---
+++ Store be_l3/About.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Questo codice è stato generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Store_be_l3
+{
+
+
+    public partial class About
+    {
+
+        /// <summary>
+        /// Controllo username_par.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl username_par;
+
+        /// <summary>
+        /// Controllo prodotto1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl prodotto1;
+
+        /// <summary>
+        /// Controllo Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Controllo prodotto2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl prodotto2;
+
+        /// <summary>
+        /// Controllo Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// Controllo prodotto3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl prodotto3;
+
+        /// <summary>
+        /// Controllo Button3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button3;
+
+        /// <summary>
+        /// Controllo prodotto4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl prodotto4;
+
+        /// <summary>
+        /// Controllo Button4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
+    }
+}
